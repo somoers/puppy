a62fcbd4628a70fc42104c6704041a304f8d2105
somoers <edsomosierra@gmail.com>
2024-01-20 19:04:15 +0800

    third commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/somoers/puppy
+
+go 1.21.6
+
+require github.com/somoers/dog v0.0.0-20240120105639-fdddb79d4654
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/somoers/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof woof woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
